bf18848c8648d28d9dbc9eef58b6c21a78dd7a45
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-04 02:22:14 -0300

    Realizando delete de registros

--- projeto-spring-data-jpa/src/test/java/projeto/spring/data/jpa/test/APPSpringDataTest.java
+++ projeto-spring-data-jpa/src/test/java/projeto/spring/data/jpa/test/APPSpringDataTest.java
@@ -12,88 +12,121 @@ import projeto.spring.data.jpa.dao.UsuarioRepository;
 import projeto.spring.data.jpa.model.UsuarioSpringData;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
+@ContextConfiguration(locations = { "classpath:META-INF/spring-config.xml" })
 public class APPSpringDataTest {
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	//@Test
 	public void testeInsert() {
-		
+
 		UsuarioSpringData usuario = new UsuarioSpringData();
-		usuario.setLogin("fabiana");
+		usuario.setLogin("daiane");
 		usuario.setSenha("123");
-		usuario.setNome("Fabiana");
-		usuario.setEmail("fi4.andrade@gmail.com");
-		usuario.setIdade(34);
-		
+		usuario.setNome("Daiane");
+		usuario.setEmail("daiane@gmail.com");
+		usuario.setIdade(27);
+
 		usuarioRepository.save(usuario);
-		
+
 		System.out.println("Usuário salvo com sucesso!");
-		
+
 		System.out.println("Usuários cadastrados -> " + usuarioRepository.count());
 	}
-	
-	//@Test
+
+	// @Test
 	public void testeConsultarPorId() {
-		
-		//O método .findById() é do tipo Optional
+
+		// O método .findById() é do tipo Optional
 		Optional<UsuarioSpringData> usuario = usuarioRepository.findById(3L);
-		
-		if(usuario.isPresent()) {
-		
-		// O método get() retorna o próprio usuário com todas as suas propriedades
-	    System.out.println("Dados do usuario: ");
-	    System.out.println("Id: " + usuario.get().getId());
-	    System.out.println("Nome: " + usuario.get().getNome());
-	    System.out.println("Login: " + usuario.get().getLogin());
-	    System.out.println("Senha: " + usuario.get().getSenha());
-	    System.out.println("Email: " + usuario.get().getEmail());
-	    System.out.println("Idade: " + usuario.get().getIdade());
-	    
-		}else {
-		System.out.println("Usuario inexistente");
+
+		if (usuario.isPresent()) {
+
+			// O método get() retorna o próprio usuário com todas as suas propriedades
+			System.out.println("Dados do usuario: ");
+			System.out.println("Id: " + usuario.get().getId());
+			System.out.println("Nome: " + usuario.get().getNome());
+			System.out.println("Login: " + usuario.get().getLogin());
+			System.out.println("Senha: " + usuario.get().getSenha());
+			System.out.println("Email: " + usuario.get().getEmail());
+			System.out.println("Idade: " + usuario.get().getIdade());
+
+		} else {
+			System.out.println("Usuario inexistente");
 		}
 	}
-	
-	//@Test
+
+	// @Test
 	public void consultarTodos() {
-		
+
 		Iterable<UsuarioSpringData> listaUsu = usuarioRepository.findAll();
-		
+
 		for (UsuarioSpringData usuarioSpringData : listaUsu) {
 			System.out.println("Dados do usuario: ");
-		    System.out.println("Id: " + usuarioSpringData.getId());
-		    System.out.println("Nome: " + usuarioSpringData.getNome());
-		    System.out.println("Login: " + usuarioSpringData.getLogin());
-		    System.out.println("Senha: " + usuarioSpringData.getSenha());
-		    System.out.println("Email: " + usuarioSpringData.getEmail());
-		    System.out.println("Idade: " + usuarioSpringData.getIdade());
-		    System.out.println("------------------------------------------------");
+			System.out.println("Id: " + usuarioSpringData.getId());
+			System.out.println("Nome: " + usuarioSpringData.getNome());
+			System.out.println("Login: " + usuarioSpringData.getLogin());
+			System.out.println("Senha: " + usuarioSpringData.getSenha());
+			System.out.println("Email: " + usuarioSpringData.getEmail());
+			System.out.println("Idade: " + usuarioSpringData.getIdade());
+			System.out.println("------------------------------------------------");
 		}
 	}
-	
-	@Test
+
+	// @Test
 	public void testeUpdate() {
-		
+
 		Optional<UsuarioSpringData> usuario = usuarioRepository.findById(10L);
-		
-		if(usuario.isPresent()) {
-			
-		   UsuarioSpringData usuAtualizar = usuario.get();
-		   
-		   // Valor a ser modificado
-		   usuAtualizar.setNome("Jair Bolsonaro");
-		   usuAtualizar.setLogin("bolsonaro");
-		   usuAtualizar.setEmail("bo4@gmail.com");
-		   
-		   //Salvar no banco de dados 
-		   usuarioRepository.save(usuAtualizar);
-		   
-		   System.out.println("Usuário modificado com sucesso!");
-			
-		}else {
+
+		if (usuario.isPresent()) {
+
+			UsuarioSpringData usuAtualizar = usuario.get();
+
+			// Valor a ser modificado
+			usuAtualizar.setNome("Jair Bolsonaro");
+			usuAtualizar.setLogin("bolsonaro");
+			usuAtualizar.setEmail("bo4@gmail.com");
+
+			// Salvar no banco de dados
+			usuarioRepository.save(usuAtualizar);
+
+			System.out.println("Usuário modificado com sucesso!");
+
+		} else {
+			System.out.println("Usuario inexistente");
+		}
+	}
+
+	// @Test
+	public void testeDeleteSimples() {
+		usuarioRepository.deleteById(2L);
+		System.out.println("Usuário deletado com sucesso!");
+	}
+
+	//@Test
+	public void testeDelete() {
+		Optional<UsuarioSpringData> usuario = usuarioRepository.findById(2L);
+
+		if (usuario.isPresent()) {
+			usuarioRepository.delete(usuario.get());
+
+			System.out.println("Usuário deletado com sucesso!");
+		} else {
+			System.out.println("Usuario inexistente");
+		}
+	}
+
+	@Test
+	public void testeDeletePorId() {
+
+		Optional<UsuarioSpringData> usuario = usuarioRepository.findById(41L);
+
+		if (usuario.isPresent()) {
+			usuarioRepository.deleteById(usuario.get().getId());
+
+			System.out.println("Usuário deletado com sucesso!");
+		} else {
 			System.out.println("Usuario inexistente");
 		}
 	}
