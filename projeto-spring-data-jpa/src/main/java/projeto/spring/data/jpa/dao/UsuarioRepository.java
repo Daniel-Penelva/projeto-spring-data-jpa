9d5b8a9441e34bbe23f6a6e56513d832e4f40375
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-03 23:12:29 -0300

    Criando nosso DAO Repository (UsuarioRepository)

---
+++ projeto-spring-data-jpa/src/main/java/projeto/spring/data/jpa/dao/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package projeto.spring.data.jpa.dao;
+
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import projeto.spring.data.jpa.model.UsuarioSpringData;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<UsuarioSpringData, Long>{
+	
+}
--- projeto-spring-data-jpa/src/test/java/projeto/spring/data/jpa/test/APPSpringDataTest.java
+++ projeto-spring-data-jpa/src/test/java/projeto/spring/data/jpa/test/APPSpringDataTest.java
@@ -2,12 +2,18 @@ package projeto.spring.data.jpa.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import projeto.spring.data.jpa.dao.UsuarioRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
 public class APPSpringDataTest {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 	
 	@Test
 	public void testeInsert() {
